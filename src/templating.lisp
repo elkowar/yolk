b807be06a21335bfd4044e613f66ae0fc5c4ab72
elkowar <5300871+elkowar@users.noreply.github.com>
2023-03-20 00:19:15 +0100

    More cleanup

--- src/templating.lisp
+++ src/templating.lisp
@@ -13,31 +13,39 @@
 ; TODO add support for conditional inclusion of blocks of text
 
 (defparameter *tests*
-  '(("bg_color = \"#ebdbb2\" # [yolk:ri] \" -> \"12\""
-     . "bg_color = \"12\" # [yolk:ri] \" -> \"12\"")
-    ("bg_color = \"#ebdbb2\" # [yolk:ri] \" -> (+ 5 2)"
-     . "bg_color = \"7\" # [yolk:ri] \" -> (+ 5 2)")
-    ("bg_color = \"#ebdbb2\" # [yolk:r] \".*?\" -> \"\\\"12\\\"\""
-     . "bg_color = \"12\" # [yolk:r] \".*?\" -> \"\\\"12\\\"\"")))
+  (list (cons "bg_color = \"#ebdbb2\" # [yolk:ri] \" -> \"12\""
+              "bg_color = \"12\" # [yolk:ri] \" -> \"12\"")
+        (cons "bg_color = \"#ebdbb2\" # [yolk:ri] \" -> (+ 5 2)"
+              "bg_color = \"7\" # [yolk:ri] \" -> (+ 5 2)")
+        (cons "bg_color = \"#ebdbb2\" # [yolk:r] \".*?\" -> \"\\\"12\\\"\""
+              "bg_color = \"12\" # [yolk:r] \".*?\" -> \"\\\"12\\\"\"")))
     ;("bg_color = \"#ebdbb2\" # [yolk:r] /\".*\"/ -> (concat \"\\\"\" colors.background \"\\\"\")"
     ; . "bg_color = \"#ff0000\" # [yolk:r] /\".*\"/ -> (concat \"\\\"\" colors.background \"\\\"\")")))
+
+
+(defun dbg (x)
+  (print (describe x))
+  x)
 
 (defun* run-replacement-code ((input string))
   (eval (read-from-string input)))
 
 
-(defun* (within -> string) ((outer string) (inner string))
+(defun* within ((outer string) (inner string))
+  (:returns string)
   (str:concat outer inner outer))
 
-(defun* (directive-replace-within -> string) ((pattern string) (input string) (replacement string))
+(defun* directive-replace-within ((pattern string) (input string) (replacement string))
+  (:returns string)
   (cl-ppcre:regex-replace (within pattern ".*?") input (within pattern replacement)))
 
-(defun* (directive-replace -> string) ((pattern string) (input string) (replacement string))
+(defun* directive-replace ((pattern string) (input string) (replacement string))
+  (:returns string)
   (cl-ppcre:regex-replace pattern input replacement))
 
-(defun* (read-directive -> (or null (function (string string string) string)))
-        ((directive string))
-  (ematch directive
+(defun* read-directive ((directive string))
+  (:returns (or null (function (string string string) string)))
+  (match directive
     ("ri" (function directive-replace-within))
     ("r" (function directive-replace))))
 
@@ -47,26 +55,26 @@
    (pattern :initarg :pattern :accessor pattern)
    (replacement :initarg :replacement :accessor replacement)))
 
-(defmethod print-object ((c yolk-call) stream)
-  (format stream "(yolk-call :directive \"~a\" :pattern \"~a\" :replacement \"~a\")"
-          (directive c) (pattern c) (replacement c)))
-
-(defun* (run-call -> string) ((call yolk-call) (input string))
+(defun* run-call ((call yolk-call) (input string))
+  (:returns string)
   (let ((dir (read-directive (directive call))))
     (funcall dir (pattern call) input (format nil "~a" (run-replacement-code (replacement call))))))
 
 
-(defun* (read-inline-yolk-call -> (or null yolk-call)) ((input string))
+(defun* read-inline-yolk-call ((input string))
+  (:returns (or null yolk-call))
   (cl-ppcre:register-groups-bind (directive pattern replacement)
       (".*\\[yolk:(.*?)\\](.*) *-> *(.*)" input)
     (make-instance 'yolk-call :directive (str:trim directive)
                               :pattern (str:trim pattern)
                               :replacement (str:trim replacement))))
 
-(defun process-text (data text)
+(defun* process-text ((data t) (text string))
+  (:returns string)
   (declare (ignore data))
-  (iter (for line in (str:lines text))
-    (collect (run-call (print (read-inline-yolk-call text)) line))))
+  (str:unlines
+   (iter (for line in (str:lines text))
+    (collect (run-call (read-inline-yolk-call text) line)))))
 
 (defun tests ()
   (format t "~%Running tests...~%")
