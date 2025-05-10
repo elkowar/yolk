d6b9f60a32db4b8a7853674b043a57cf14e3dc51
elkowar <dev@elkowar.dev>
2025-05-10 21:37:39 +0200

    refactor: cleanup implementation of rhai error message handling

--- src/script/rhai_error.rs
+++ src/script/rhai_error.rs
@@ -48,35 +48,14 @@ impl RhaiError {
     }
 
     pub fn from_rhai(engine: &rhai::Engine, source_code: &str, err: rhai::EvalAltResult) -> Self {
-        let position = err.position();
-        let mut span = 0..0;
-        if let Some(line_nr) = position.line() {
-            // TODO: this won't work with \r\n, _or will it_? *vsauce music starts playing*
-            let offset_start = source_code
-                .split_inclusive('\n')
-                .take(line_nr - 1)
-                .map(|x| x.len())
-                .sum::<usize>();
-            span = if let Some(within_line) = position.position() {
-                offset_start + within_line..offset_start + within_line + 1
-            } else {
-                let offset_end = offset_start
-                    + source_code
-                        .lines()
-                        .nth(line_nr - 1)
-                        .map(|x| x.len())
-                        .unwrap_or_default();
-                let indent = source_code[offset_start..]
-                    .chars()
-                    .take_while(|x| x.is_whitespace())
-                    .count();
-                offset_start + indent..offset_end
-            };
-        }
-        if span.start >= source_code.len() {
-            span = source_code.len() - 1..source_code.len();
-        }
-        let rhai_error = enhance_rhai_error(engine, err);
+        let span = span_from_rhai_eval_result(source_code, &err);
+        let rhai_error = match err {
+            rhai::EvalAltResult::ErrorFunctionNotFound(signature, _position) => {
+                Self::from_rhai_function_not_found(engine, signature)
+            }
+            other => Self::RhaiError(other),
+        };
+
         Self::SourceError {
             span,
             origin: Box::new(rhai_error),
@@ -89,40 +68,72 @@ impl RhaiError {
             miette::NamedSource::new(name.to_string(), source.to_string()).with_language("Rust"),
         )
     }
+
+    /// Generate a [`RhaiError::RhaiFunctionNotFound`] for a [`rhai::EvalAltResult::ErrorFunctionNotFound`],
+    /// adding hints on which similar functions do exist.
+    ///
+    /// You most definitely want to use [`RhaiError::from_rhai_with_source`] instead, as it also adds span information.
+    /// This function is only used internally.
+    fn from_rhai_function_not_found(engine: &rhai::Engine, signature: String) -> RhaiError {
+        let actual_fn_name = signature.split(['(', ' ']).next().unwrap_or("");
+        let mut candidates = engine.collect_fn_metadata(
+            None,
+            |info| {
+                let distance = strsim::levenshtein(actual_fn_name, info.metadata.name.as_str());
+                (distance < 3).then(|| {
+                    let candidate = info
+                        .metadata
+                        .gen_signature(|x| engine.map_type_name(x).into());
+                    (candidate, distance)
+                })
+            },
+            true,
+        );
+        candidates.sort_by_key(|(_, distance)| *distance);
+        let had_perfect_match = candidates.iter().any(|(_, d)| *d == 0);
+        if had_perfect_match {
+            candidates = candidates
+                .into_iter()
+                .take_while(|(_, distance)| *distance == 0)
+                .collect();
+        }
+
+        RhaiError::RhaiFunctionNotFound {
+            signature,
+            perfect_match: had_perfect_match,
+            candidates: candidates.into_iter().map(|(x, _)| x).collect(),
+        }
+    }
 }
 
-fn enhance_rhai_error(engine: &rhai::Engine, err: rhai::EvalAltResult) -> RhaiError {
-    let rhai::EvalAltResult::ErrorFunctionNotFound(signature, _) = err else {
-        return RhaiError::RhaiError(err);
-    };
-    let actual_fn_name = signature.split(['(', ' ']).next().unwrap_or("");
-    let mut candidates = engine.collect_fn_metadata(
-        None,
-        |info| {
-            let distance = strsim::levenshtein(actual_fn_name, info.metadata.name.as_str());
-            if distance < 3 {
-                let candidate = info
-                    .metadata
-                    .gen_signature(|x| engine.map_type_name(x).into());
-                Some((candidate, distance))
-            } else {
-                None
-            }
-        },
-        true,
-    );
-    candidates.sort_by_key(|(_, distance)| *distance);
-    let had_perfect_match = candidates.iter().any(|(_, d)| *d == 0);
-    if had_perfect_match {
-        candidates = candidates
-            .into_iter()
-            .take_while(|(_, distance)| *distance == 0)
-            .collect();
+fn span_from_rhai_eval_result(source_code: &str, err: &rhai::EvalAltResult) -> Range<usize> {
+    let position = err.position();
+    let mut span = 0..0;
+    if let Some(line_nr) = position.line() {
+        // TODO: this won't work with \r\n, _or will it_? *vsauce music starts playing*
+        let offset_start = source_code
+            .split_inclusive('\n')
+            .take(line_nr - 1)
+            .map(|x| x.len())
+            .sum::<usize>();
+        span = if let Some(within_line) = position.position() {
+            offset_start + within_line..offset_start + within_line + 1
+        } else {
+            let offset_end = offset_start
+                + source_code
+                    .lines()
+                    .nth(line_nr - 1)
+                    .map(|x| x.len())
+                    .unwrap_or_default();
+            let indent = source_code[offset_start..]
+                .chars()
+                .take_while(|x| x.is_whitespace())
+                .count();
+            offset_start + indent..offset_end
+        };
     }
-
-    RhaiError::RhaiFunctionNotFound {
-        signature,
-        perfect_match: had_perfect_match,
-        candidates: candidates.into_iter().map(|(x, _)| x).collect(),
+    if span.start >= source_code.len() {
+        span = source_code.len() - 1..source_code.len();
     }
+    span
 }
