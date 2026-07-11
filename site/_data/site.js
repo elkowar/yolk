74d4488e19f374f00102f8a9cd6c1cd6abaa246f
ElKowar <dev@elkowar.dev>
2026-07-11 16:08:20 +0200

    Improve responsiveness and social preview

--- site/_data/site.js
+++ site/_data/site.js
@@ -2,8 +2,9 @@ export default {
   name: "Yolk",
   packageName: "yolk_dots",
   binaryName: "yolk",
-  description: "Painfree templated dotfile management",
+  description: "templated dotfile management without template files",
   repository: "https://github.com/elkowar/yolk",
+  origin: "https://elkowar.github.io",
   homepage: "https://elkowar.github.io/yolk",
   license: "MIT OR Apache-2.0",
 };
